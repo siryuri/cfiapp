e38058ecbe50d940f19f1e6e25b2f2c15f4f6cd3
siryuri <siryuri@gmail.com>
2014-06-06 16:36:11 -0700

    mocks for project initialization

---
+++ projects/scripts/app.js
@@ -0,0 +1,14 @@
+/**
+ * @can someone make a proper app.js file? mine is horrible, sorry! check mocks to know what to connect it to
+ */
+
+'use strict';
+ 
+ 
+angular.module('projectApp', ['studentDetailApp.controllers']).
+ 
+  config(['$routeProvider', function($routeProvider,ProjectController) {
+ 
+    
+ 
+  }]);
